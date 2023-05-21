70e49cabd84500e3f8715fda89fc65cdbc56f1eb
Fábio Damasceno Silva <fabiodamasceno4@hotmail.com>
2023-05-21 16:29:05 -0300

    Terminado o primeiro curso

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -23,12 +24,14 @@ import med.voll.api.endereco.Endereco;
 public class Medico {
 
     public Medico(DadosCadastroMedico dados) {
+    	this.ativo=Boolean.TRUE;
     	this.nome=dados.nome();
     	this.telefone=dados.telefone();
     	this.email=dados.email();
     	this.crm=dados.crm();
     	this.especialidade=dados.especialidade();
     	this.endereco=new Endereco(dados.endereco());
+    	
 		
 	}
 
@@ -38,11 +41,30 @@ public class Medico {
     private String telefone;
     private String email;
     private String crm;
+    private Boolean ativo;
 
     @Enumerated(EnumType.STRING)
     private Especialidade especialidade;
 
     @Embedded
     private Endereco endereco;
+
+	public void atualizarInformacoes(@Valid DadosAtualizacaoMedico dados) {
+		if (dados.nome()!=null) {
+		this.nome=dados.nome();
+		}
+		if (dados.telefone()!=null) {
+			this.telefone=dados.telefone();
+		}
+		if (dados.endereco()!=null) {
+			this.endereco.atualizarInformacoes(dados.endereco());
+		}
+		
+	}
+
+	public void excluir() {
+		this.ativo=false;
+		
+	}
     
 }
---
+++ src/main/resources/db/migration/V4__alter-table-medicos-add-column-ativo.sql
@@ -0,0 +1,2 @@
+alter table vollmed_api.medicos add ativo tinyint;
+update medicos set ativo=1;
